c3d61baac793bc0174cb1532bfd9767397c6dd72
Alexander Marek <alexander.marek@outlook.com>
2026-03-09 12:11:17 +0100

    - added IsVisible property to SideMenuItems

--- src/AvaloniaInside.Shell/Data/SideMenuItem.cs
+++ src/AvaloniaInside.Shell/Data/SideMenuItem.cs
@@ -1,10 +1,30 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Avalonia.Media;
 
 namespace AvaloniaInside.Shell.Data;
 
-public class SideMenuItem : IItem
+public class SideMenuItem : IItem, INotifyPropertyChanged
 {
+	private bool _isVisible = true;
+
 	public string Title { get; set; }
 	public string Path { get; set; }
 	public IImage? Icon { get; set; }
+
+	public bool IsVisible
+	{
+		get => _isVisible;
+		set
+		{
+			if (_isVisible == value) return;
+			_isVisible = value;
+			OnPropertyChanged();
+		}
+	}
+
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+		=> PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
